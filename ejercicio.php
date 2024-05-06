e67faeb30898529bbdff8227aff5dbddaa94b800
Vargas <jeanpaulfuentesvargas@gmail.com>
2024-05-05 20:35:11 -0600

    primera parte

---
+++ ejercicio.php
@@ -0,0 +1,16 @@
+<?php 
+
+class DispositivoElectronico {
+protected $nombre;
+protected $consumoElectrico;
+
+public function __construct($nombre,$consumoElectrico) {
+$this->nombre = $nombre;
+$this->consumoElectrico = $consumoElectrico
+}
+
+
+
+
+
+}
